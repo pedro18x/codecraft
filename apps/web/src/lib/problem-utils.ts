7da2147e05083d35f7255dc5314041899151f36a
Pedro <pedroernestovogado@gmail.com>
2026-03-13 00:33:02 -0300

    refactor:dead-code-removal

--- apps/web/src/lib/problem-utils.ts
+++ apps/web/src/lib/problem-utils.ts
@@ -6,7 +6,3 @@ export function getProblemSlug(problem: ProblemWithSlug): string {
   if (problem.slug && problem.slug.length > 0) return problem.slug
   return problem.title.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '')
 }
-
-export function findProblemBySlug<T extends ProblemWithSlug>(problems: T[], slug: string): T | undefined {
-  return problems.find((p) => getProblemSlug(p) === slug)
-}
